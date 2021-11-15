1cfb4a0ca360cfad9fb102763eaa1bc0fb8e712d
milya <shimayr@mail.ru>
2021-11-15 20:33:18 +0300

    дз урок5

---
+++ lesson5exer4.py
@@ -0,0 +1,12 @@
+translations = {
+    "One": "Один",
+    "Two": "Два",
+    "Three": "Три",
+    "Four": "Четыре"}
+converted_rows = []
+with open("task04.txt") as input_data:
+    for row in input_data:
+        name, value = row.split(' - ')
+        converted_rows.append(f"{translations[name]} - {value}")
+with open("task04_ru.txt", "w") as output_data:
+    output_data.writelines(converted_rows)
